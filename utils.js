c4ef0186f7f1eb6dbb1e4612bb677753f0afbc52
kiran <kirankumar.charkaman@cdk.com>
2017-06-15 09:09:15 +0530

    add created date and refactored utils

---
+++ utils.js
@@ -0,0 +1,30 @@
+﻿var utils = angular.module('utils', []);
+
+
+utils.filter('trimText', function (defaultTrimLength) {
+    return function (data, length) {
+        length = length || defaultTrimLength;
+        return data.length > length ? data.substr(0, length) + '...' : data;
+    }
+});
+utils.filter('toMoment', function (momentApi) {
+    return function (data) {
+        return momentApi(data).fromNow();
+    }
+});
+
+utils.filter("closedCount", function ($filter) {
+    var builtInFilter = $filter('filter');
+    return function (bugs) {
+
+        return builtInFilter(bugs, { isClosed: true }).length;
+    }
+});
+//bugTrackerApp.filter("closedCount", function () {
+
+//    return function (bugs) {
+//        return bugs.reduce(function (total, bug) {
+//            return bug.isClosed ? ++total : total;
+//        }, 0);
+//    }
+//});
